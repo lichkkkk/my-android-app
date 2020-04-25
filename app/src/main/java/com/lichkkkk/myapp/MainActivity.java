ac801ac011d8a9569a4878e893b72c8ebeaa19e0
lichkkkk <changli0914@gmail.com>
2020-04-25 23:50:18 +0100

    Make the links to work

--- app/src/main/java/com/lichkkkk/myapp/MainActivity.java
+++ app/src/main/java/com/lichkkkk/myapp/MainActivity.java
@@ -1,32 +1,49 @@
 package com.lichkkkk.myapp;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.textclassifier.TextClassificationManager;
 import android.view.textclassifier.TextClassifier;
 import android.view.textclassifier.TextLinks;
-import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MyAppMainActivity";
 
-    private Button linkifyButton;
-    private Button resetButton;
+    private ExecutorService executorService;
 
-
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        linkifyButton = findViewById(R.id.linkify_button);
-        linkifyButton.setOnClickListener(view -> onLinkify());
-        resetButton = findViewById(R.id.reset_button);
-        resetButton.setOnClickListener(v -> onReset());
+        findViewById(R.id.linkify_button).setOnClickListener(view -> onLinkify());
+        findViewById(R.id.reset_button).setOnClickListener(v -> onReset());
+
+        executorService = Executors.newSingleThreadExecutor();
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        executorService.shutdown();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     void onLinkify() {
         TextClassificationManager textClassificationManager = getSystemService(TextClassificationManager.class);
         if (textClassificationManager == null) {
@@ -34,13 +51,35 @@ public class MainActivity extends AppCompatActivity {
         }
         TextClassifier textClassifier = textClassificationManager.getTextClassifier();
         TextView textView = findViewById(R.id.linkify_textview);
-        SpannableString text = SpannableString.valueOf(textView.getText());
-        TextLinks.Request textLinksRequest = new TextLinks.Request.Builder(text).build();
-        // TODO: Call generateLinks() on a worker thread.
-        TextLinks textLinks = textClassifier.generateLinks(textLinksRequest);
-        textLinks.apply(text, TextLinks.APPLY_STRATEGY_REPLACE, null);
-        textView.setText(text);
-        // TODO: It seems we cannot click the links?
+        // textClassifier = textView.getTextClassifier();
+        // Linkify.addLinks(textView, Linkify.ALL);
+        CharSequence textViewCharSequence = textView.getText();
+        SpannableString text = new SpannableString(textViewCharSequence);
+        List<String> entitiesToLinkify = Arrays.asList(
+                TextClassifier.TYPE_DATE_TIME,
+                TextClassifier.TYPE_URL);
+        TextLinks.Request.Builder builder = new TextLinks.Request.Builder(text);
+        builder.setEntityConfig(
+                new TextClassifier.EntityConfig.Builder()
+                        .setExcludedTypes(entitiesToLinkify)
+                        .build());
+        TextLinks.Request textLinksRequest = builder.build();
+
+        Future<TextLinks> textLinksFuture = executorService.submit(
+                () -> textClassifier.generateLinks(textLinksRequest)
+        );
+        try {
+            TextLinks textLinks = textLinksFuture.get();
+            int status = textLinks.apply(text, TextLinks.APPLY_STRATEGY_REPLACE, null);
+            textView.setLinksClickable(true);
+            textView.setTextIsSelectable(false);
+            textView.setMovementMethod(LinkMovementMethod.getInstance());
+            textView.setText(text);
+            textView.setFocusable(true);
+            textView.setFocusableInTouchMode(true);
+        } catch (ExecutionException | InterruptedException e) {
+            Log.e(TAG, "error generating links", e);
+        }
     }
 
     void onReset() {
