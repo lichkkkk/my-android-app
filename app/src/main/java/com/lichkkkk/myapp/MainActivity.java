24e3524eb7b77298e40f18db42d501934d8501db
lichkkkk <changli0914@gmail.com>
2020-04-26 14:47:33 +0100

    Add buttons for TextClassifier.classifyText

--- app/src/main/java/com/lichkkkk/myapp/MainActivity.java
+++ app/src/main/java/com/lichkkkk/myapp/MainActivity.java
@@ -1,13 +1,16 @@
 package com.lichkkkk.myapp;
 
+import android.app.PendingIntent;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.method.LinkMovementMethod;
 import android.util.Log;
+import android.view.textclassifier.TextClassification;
 import android.view.textclassifier.TextClassificationManager;
 import android.view.textclassifier.TextClassifier;
 import android.view.textclassifier.TextLinks;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
@@ -17,8 +20,6 @@ import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 
@@ -34,7 +35,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.linkify_button).setOnClickListener(view -> onLinkify());
-        findViewById(R.id.reset_button).setOnClickListener(v -> onReset());
+        findViewById(R.id.linkify_reset_button).setOnClickListener(v -> onLinkifyReset());
+        findViewById(R.id.classify_button).setOnClickListener(v -> onClassify());
+        findViewById(R.id.classify_reset_button).setOnClickListener(v -> onClassifyReset());
 
         executorService = MoreExecutors.listeningDecorator(Executors.newSingleThreadExecutor());
     }
@@ -47,7 +50,8 @@ public class MainActivity extends AppCompatActivity {
 
     @RequiresApi(api = Build.VERSION_CODES.Q)
     void onLinkify() {
-        TextClassificationManager textClassificationManager = getSystemService(TextClassificationManager.class);
+        TextClassificationManager textClassificationManager =
+                getSystemService(TextClassificationManager.class);
         if (textClassificationManager == null) {
             return;
         }
@@ -57,16 +61,7 @@ public class MainActivity extends AppCompatActivity {
         // Linkify.addLinks(textView, Linkify.ALL);
         CharSequence textViewCharSequence = textView.getText();
         SpannableString text = new SpannableString(textViewCharSequence);
-        List<String> entitiesToLinkify = Arrays.asList(
-                TextClassifier.TYPE_DATE_TIME,
-                TextClassifier.TYPE_URL);
-        TextLinks.Request.Builder builder = new TextLinks.Request.Builder(text);
-        builder.setEntityConfig(
-                new TextClassifier.EntityConfig.Builder()
-                        .setExcludedTypes(entitiesToLinkify)
-                        .build());
-        TextLinks.Request textLinksRequest = builder.build();
-
+        TextLinks.Request textLinksRequest = new TextLinks.Request.Builder(text).build();
         ListenableFuture<TextLinks> textLinksFuture = executorService.submit(
                 () -> textClassifier.generateLinks(textLinksRequest)
         );
@@ -82,8 +77,46 @@ public class MainActivity extends AppCompatActivity {
         }), MoreExecutors.directExecutor());
     }
 
-    void onReset() {
+    void onLinkifyReset() {
         TextView textView = findViewById(R.id.linkify_textview);
         textView.setText(R.string.linkify_test_text);
+    }
+
+    void onClassify() {
+        TextView inputTextView = findViewById(R.id.classify_input_textview);
+        TextClassifier textClassifier = inputTextView.getTextClassifier();
+        CharSequence inputText = inputTextView.getText();
+        TextClassification.Request request = new TextClassification.Request.Builder(
+                inputText, 0, inputText.length() - 1
+        ).build();
+        ListenableFuture<TextClassification> textClassificationFuture = executorService.submit(
+                () -> textClassifier.classifyText(request)
+        );
+        textClassificationFuture.addListener(() -> runOnUiThread(() -> {
+            try {
+                TextClassification textClassification = textClassificationFuture.get();
+                if (textClassification.getEntityCount() == 0) {
+                    return;
+                }
+                Button classifiedActionButton = findViewById(R.id.classify_action_button);
+                classifiedActionButton.setText(textClassification.getEntity(0));
+                classifiedActionButton.setOnClickListener(v -> {
+                    try {
+                        textClassification.getActions().get(0).getActionIntent().send();
+                    } catch (PendingIntent.CanceledException e) {
+                        Log.e(TAG, "error sending intent");
+                    }
+                });
+            } catch (ExecutionException | InterruptedException e) {
+                Log.e(TAG, "error classifying text", e);
+            }
+        }), MoreExecutors.directExecutor());
+    }
+
+    void onClassifyReset() {
+        Button classifiedActionButton = findViewById(R.id.classify_action_button);
+        classifiedActionButton.setText(R.string.classify_action_button_default_name);
+        classifiedActionButton.setOnClickListener(v -> {
+        });
     }
 }
